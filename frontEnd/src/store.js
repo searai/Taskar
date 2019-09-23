b1067c9acd7432e3c0fc5c8f90e93f887d549aa5
searai <johnpoconnor4@gmail.com>
2019-09-23 17:45:01 +0100

    Update store.js

--- frontEnd/src/store.js
+++ frontEnd/src/store.js
@@ -1,13 +1,14 @@
 import vue from "vue"
 import vuex from "vuex"
 import {router} from "./main.js"
-
+import axios from "axios"
 vue.use(vuex)
 
 const store = new vuex.Store({
     state: {
       userName: null,
-      token: null
+      token: null,
+      listOfComments: []
 
     },
     getters:{
@@ -16,6 +17,9 @@ const store = new vuex.Store({
         },
         getToken(state){
           return state.token
+        },
+        getListOfComments(state){
+          return state.listOfComments
         }
     },
     mutations: {
@@ -30,14 +34,68 @@ const store = new vuex.Store({
         state.userName = payload.userName
         state.token = payload.token
       },
+      updateListOfComments(state, payload){
+        state.listOfComments = payload
+      }
     },
     actions:{
       setLogOutTimer({commit}){
         setTimeout(()=>{
           commit("logout")
         },60*60*1000)    
+    },
+    getAllComments: async function({commit}){
+      try{
+        const response = await axios.get("comment/getComments")
+        commit("updateListOfComments", response.data)
+      }catch(e){
+        if(e.response.status == 500){
+          router.replace({path:"/serverError"})
+        }
+      }
+    },
+    updateCommnent({dispatch}, {id, body}){
+      axios.put(`/comment/update/${id}`, {body})
+      .then(()=>{
+        dispatch("getAllComments")
+      })
+      .catch((e)=>{
+        if(e.response.status == 500){
+          router.replace({path:"/serverError"})
+        }
+      })
+    },
+    postComment({dispatch}, {body, author, linkedTo} ){
+      axios.post("/comment/add", {body, author, linkedTo})
+      .then(()=>{
+        dispatch("getAllComments")
+      }).catch((e)=>{
+        if(e.response.status == 500){
+          router.replace({path:"/serverError"})
+        }
+      })
+    },
+    deleteComment({dispatch}, {removeMany, id}){
+      if(removeMany){
+        axios.delete(`/comment/removeMany/${id}`)
+        .then(()=>{
+          dispatch("getAllComments")
+        }).catch(e=>{
+          if(e.response.status == 500){
+            router.replace({path:"/serverError"})
+          }
+        })
+      }else{
+        axios.delete(`/comment/removeOne/${id}`)
+        .then(()=>{
+          dispatch("getAllComments")
+        }).catch(e=>{
+          if(e.response.status == 500){
+            router.replace({path:"/serverError"})
+          }
+        })
+      }
     }
-  
   }
 })
 
